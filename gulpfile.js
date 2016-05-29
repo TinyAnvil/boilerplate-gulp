3edfd6f4905e6c3b59d45e790ea5b79cdbcc20d5
Tyler van der Hoeven <hi@tyvdh.com>
2016-05-29 12:26:35 -0400

    lots of improvements, fixes and updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,74 @@
 var gulp = require('gulp'),
-    ghPages = require('gulp-gh-pages'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    cssnano = require('gulp-cssnano'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    livereload = require('gulp-livereload'),
-    newer = require('gulp-newer'),
     globbing = require('gulp-css-globbing'),
-    combineMq = require('gulp-combine-mq');
+    sourcemaps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
+    flatten = require('gulp-flatten'),
+    newer = require('gulp-newer'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    connect = require('gulp-connect-php'),
+    livereload = require('gulp-livereload');
 
-
-// ROOT TASKS // ---------------------------------------------------------
-// Main style task  
 gulp.task('css', function() {
   return gulp.src('dev/sass/application.scss')
+    .pipe(sourcemaps.init())
     .pipe(globbing({extensions: '.scss'}))
-    .pipe(sass())
-    .on('error', handleError)
-    .pipe(combineMq({beautify: false}))
-    .pipe(autoprefixer({cascade: false})) // auto prefix
-    .pipe(cssnano()) // minify everything
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false}))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('public/css'));
 });
 
-// Main Javascript task
-gulp.task('js', function() {  
-  return gulp.src('dev/js/**/*.js')
-    .pipe(newer('public/js'))
+gulp.task('vendor-js', function() {
+  return gulp.src('dev/js/vendor/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('application-vendor.js'))
     .pipe(uglify())
-    .on('error', handleError)
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('public/js'));
 });
 
-// Main image task
+gulp.task('js', function() {
+  return gulp.src(['dev/js/*/*.js', 'dev/js/*.js', '!dev/js/vendor/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('application.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/js'));
+});
+
 gulp.task('img', function() {
   return gulp.src('dev/img/**/*.{jpg,jpeg,png,gif,svg,ico}')
+    .pipe(flatten())
     .pipe(newer('public/img'))
-    .pipe(imagemin({ 
+    .pipe(imagemin({
       optimizationLevel: 5,
-      progressive: true, 
+      progressive: true,
       interlaced: true,
       svgoPlugins: [{
         collapseGroups: false,
         removeViewBox: false
       }]
     }))
-    .on('error', handleError)
     .pipe(gulp.dest('public/img'));
 });
 
-// Publish github page
-gulp.task('deploy', function() {
-  return gulp.src('public/**/*')
-    .pipe(ghPages());
-});
-
-
-// FUNCTIONS // ---------------------------------------------------------
-// Initial start function
-gulp.task('start', ['img'], function() {
-  gulp.start('js', 'css');
-});
-
-// Watch function
-gulp.task('watch', ['start'], function() {
+gulp.task('connect', function() {
   gulp.watch('dev/sass/**/*.scss', ['css']);
-  gulp.watch('dev/js/**/*.js', ['js']);
+  gulp.watch('dev/js/vendor/*.js', ['vendor-js']);
+  gulp.watch(['dev/js/**/*.js', '!dev/js/vendor/*.js'], ['js']);
   gulp.watch('dev/img/**/*.{jpg,jpeg,png,gif,svg,ico}', ['img']);
  
   livereload.listen();
-  gulp.watch(['public/*.html', 'public/js/**/*.js', 'public/img/**/*.{jpg,jpeg,png,gif,svg,ico}', 'public/css/*.css']).on('change', livereload.changed);
+
+  gulp.watch(['public/*.html', 'prod/js/*.js', 'prod/img/**/*.{jpg,jpeg,png,gif,svg,ico}', 'prod/css/*.css']).on('change', livereload.changed);
+
+  connect.server({
+    base: 'public/',
+    keepalive: false
+  });
 });
 
-// Default function
-gulp.task('default', ['watch']);
-
-// Error reporting function
-function handleError(err) {
-  console.log(err.toString());
-  this.emit('end');
-}
+gulp.task('default', ['css', 'vendor-js', 'js', 'img', 'connect']);
